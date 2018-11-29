9954a158c1a8d769c2d8697039d9df0ab9fa7d6a
Sean Porter <portertech@gmail.com>
2018-11-29 10:31:40 -0800

    hello world! bundle gem sensu-install
    Signed-off-by: Sean Porter <portertech@gmail.com>

---
+++ lib/sensu/install.rb
@@ -0,0 +1,7 @@
+require "sensu/install/version"
+
+module Sensu
+  module Install
+    # Your code goes here...
+  end
+end
---
+++ lib/sensu/install/version.rb
@@ -0,0 +1,5 @@
+module Sensu
+  module Install
+    VERSION = "0.1.0"
+  end
+end
